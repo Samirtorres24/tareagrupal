d201cc880a99c9c3545bd922ec5f8a47f50eb891
TORRES <TORRES@TORRES.mshome.net>
2021-07-16 21:10:46 -0600

    commit de tarea grupal-

---
+++ PRUEBAROPOSITORIOREMOTO/src/pruebaropositorioremoto/PRUEBAROPOSITORIOREMOTO.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaropositorioremoto;
+
+/**
+ *
+ * @author TORRES
+ */
+public class PRUEBAROPOSITORIOREMOTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        private String id;
+	private String nombre;
+	private String apellidos;
+	private String especialidad;
+	
+	public Medico(String id, String nombre, String apellidos, String especialidad) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.especialidad = especialidad;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+	
+	/**
+	 * Muestra por consola todos los datos del Medico
+	 */
+	public void mostrar() {
+		System.out.println("\n\t---------------");
+		System.out.println("ID Medico: " + id);
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("Especialidad: " + especialidad);
+		System.out.println("\t---------------");
+    }
+    
+}
